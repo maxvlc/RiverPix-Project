2396b65b3297368ca6b42fc0eb4fd1685446ef25
maxvlc <maxcar@gmail.com>
2014-08-18 11:12:06 +0200

    Adding comments to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
 	mount_uploader :image, PostImageUploader
   paginates_per 5
+  has_many :comments, dependent: :destroy
 
   def timestamp
     created_at.strftime('%d %B %Y %H:%M:%S')
