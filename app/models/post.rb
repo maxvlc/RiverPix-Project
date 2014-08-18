537171a4af6c655fd50ddd7acd24cb1137621107
maxvlc <maxcar@gmail.com>
2014-08-18 11:08:46 +0200

    Adding kaminari configuration

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,8 @@
 class Post < ActiveRecord::Base
-	
 	mount_uploader :image, PostImageUploader
+  paginates_per 5
 
-	def timestamp
-    created_at.strftime('%d %B %Y %H:%M')
+  def timestamp
+    created_at.strftime('%d %B %Y %H:%M:%S')
   end
 end
