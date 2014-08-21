20bcf28c2baa3801baffe35b06f10ec44b1524ea
maxvlc <maxcar@gmail.com>
2014-08-21 11:42:24 +0200

    Ending with CSS styles

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,8 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
+
+  
+  def timestamp
+    created_at.strftime('%d %B %Y %H:%M:%S')
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,13 @@ class Post < ActiveRecord::Base
 	mount_uploader :image, PostImageUploader
   has_many :comments, dependent: :destroy
 
+	validates :title, presence: true, length: { minimum: 5 }
+
   def timestamp
     created_at.strftime('%d %B %Y %H:%M:%S')
+  end
+
+  def comments_for_post
+    comments.count
   end
 end
