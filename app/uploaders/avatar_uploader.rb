1af1d68ae4eeb391de2f7da0b8b0fc78721cac89
maxvlc <maxcar@gmail.com>
2014-08-25 13:23:59 +0200

    Heroku config uploaders

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -9,8 +9,4 @@ class AvatarUploader < CarrierWave::Uploader::Base
   def extension_white_list
     %w(jpg jpeg gif png)
   end
-
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
 end
